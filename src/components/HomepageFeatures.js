601cbcc868594b73653837aeed6660f7921b05fd
Sam Bauch <sbauch@gmail.com>
2021-07-30 15:20:38 -0400

    imagery

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -5,7 +5,7 @@ import styles from './HomepageFeatures.module.css';
 const FeatureList = [
   {
     title: 'OSS Contracts',
-    img: require('../../static/img/01.png').default,
+    img: require('../../static/img/1.png').default,
     description: (
       <>
         Inherit our open-source ERC721 contracts to make your NFTs compatible with the Wrassling Arena
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: 'Multi-Chain',
-    img: require('../../static/img/02.png').default,
+    img: require('../../static/img/2.png').default,
     description: (
       <>
         Wrassling happens on Matic - we provide contracts and documentation for Matic-exclusive and multi-chain NFTs
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: 'Free',
-    img: require('../../static/img/03.png').default,
+    img: require('../../static/img/3.png').default,
     description: (
       <>
         NFT projects can join the Wrassling Arena at no cost, and our developer tools allow your team to start wrassling in hours
