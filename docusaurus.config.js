0b33a759b2d22505c2b835c0614242f323f2767b
Sam Bauch <sbauch@gmail.com>
2021-08-02 11:02:28 -0400

    update repo links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -101,13 +101,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/0xEssential/wrasslers-docs/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/0xEssential/wrasslers-docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
